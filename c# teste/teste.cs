05a1faae871869fad1af4c9b918dff036b4baf1e
mayyzena012 <143430528+mayyzenacs@users.noreply.github.com>
2023-10-01 12:33:04 -0300

    Create teste.cs

---
+++ c# teste/teste.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Diagnostics.Contracts;
+
+namespace armazem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+        
+        Console.WriteLine("hello");
+        String nome = Console.ReadLine();
+
+        Console.WriteLine("Hello, welcome " + nome);
+
+        }
+
+
+
+
+
+    }
+
+}
