61658ed64ff84fbac58e30c85e80ee38d0798bed
mayyzenacs <143430528+mayyzenacs@users.noreply.github.com>
2023-10-17 21:57:57 -0300

    Create paton.py

---
+++ paton.py
@@ -0,0 +1,17 @@
+from random import choice
+
+peito = ["supino vertical","supino reto/reclinado/inclinado","peck deck","crossover","pullover"]
+costas = ["pulley da frente","pulley triangulo","pulley romano","remada baixa","remada cavalinho","remada unilateral","remada curvada","pulldown"]
+ombros = ["desenvolvimento","elevação lateral","elevação frontal","remada alta","peck deck invertido"]
+biceps = ["rosca direita halter","rosca direta barra","rosca concentrada","rosca martelo","rosca scott","rosca polia corda","rosca inclinado"]
+triceps = ["triceps pulley","triceps pulley invertido","triceps corda","triceps testa","triceps frances","triceps banco"]
+panturrilha = ["gemeos sentado","gemeos leg press"]
+perna = ["cadeira extensora","mesa flexora","leg press","adutora","abduora","agachamento livre","agachamento hack","agachamento sumo","agachamento smith","avanço","sassy squat"]
+abdomen = [""]
+gluteos = [""]
+cardio = [""]
+
+ficha = {}
+
+print(choice(peito))
+
